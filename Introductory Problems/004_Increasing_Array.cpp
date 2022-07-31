fa149bcc514e1eb44e1b9343ed9c41f205bfa706
Devendra Singh Rana <sachinrajput10101010@gmail.com>
2022-07-31 23:40:03 +0530

    Create 004_Increasing_Array.cpp

---
+++ Introductory Problems/004_Increasing_Array.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+#define int long long
+using namespace std;
+
+void solve(){
+    
+    int n;
+    cin>>n;
+    int arr[n];
+    long long diff = 0;
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    int minEle = arr[0];
+    for(int i=1;i<n;i++){
+
+        if(arr[i]<minEle){
+            diff += minEle-arr[i];
+        }
+        minEle = arr[i];
+
+    }
+    cout<<diff<<endl;
+  
+}
+
+int32_t main(){
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+ 
+  
+  solve();
+ 
+  return 0;
+}
