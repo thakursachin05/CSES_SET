b789055abd318aa96b70e9409cbc300967fcec7a
Devendra Singh Rana <sachinrajput10101010@gmail.com>
2022-07-31 23:40:06 +0530

    Create temp.cpp

---
+++ Templetes/temp.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+#define int long long
+using namespace std;
+
+void solve(){
+  
+  
+}
+
+int32_t main(){
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  int t;
+  cin>>t;
+  
+  solve();
+ 
+  return 0;
+}
