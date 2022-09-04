cd3c5bab5d4d5bc22a33d0e8bf122dbede93ded1
Devendra Singh Rana <sachinrajput10101010@gmail.com>
2022-09-04 15:43:53 +0530

    Update temp.cpp

--- Templetes/temp.cpp
+++ Templetes/temp.cpp
@@ -5,22 +5,39 @@ typedef vector<int> vi;
 typedef pair<int,int> pi;
 #define first F
 #define second S
-#define push_back PB
+// #define push_back PB
 #define make_pair MP
 #define rep(i,a,b) for(int i=a;i<b;i++)
+#define rev(i,a,b) for(int i=a;i>=b;i--)
+#define mod 1000000007
+ll n,x,y,k;
 
-void solve(){
+
   
-  
+void solve(){ 
+
 }
 
+
 int32_t main(){
+
   ios_base::sync_with_stdio(false);
   cin.tie(NULL);
-  int t;
-  cin>>t;
-  
-  solve();
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+#endif
+
+  bool flag = 1;
+  ll t = 1;
+  if(flag)
+    cin>>t;
+
+  while(t--){
+    solve();
+  }
+
  
   return 0;
 }
