123cff35e61434d1bf0441718864b6a47011b66f
Devendra Singh Rana <sachinrajput10101010@gmail.com>
2022-09-06 22:05:20 +0530

    Create arrayDescription.cpp

---
+++ Dynamic Programming/arrayDescription.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int,int> pi;
+#define first F
+#define second S
+// #define push_back PB
+#define make_pair MP
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define rev(i,a,b) for(int i=a;i>=b;i--)
+#define mod 1000000007
+
+const ll inf = 1e17;
+ll n,m,x,y,k;
+
+
+void solve(){ 
+    cin>>n;
+    vector<int> arr;
+    rep(i,0,n) cin>>arr[i];
+
+    vector<vector<int>> dp(n+1,vector<int>(101,0));
+    
+
+
+}
+
+
+int32_t main(){
+
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+#endif
+
+  bool flag = 0;
+  ll t = 1;
+  if(flag)
+    cin>>t;
+    
+  while(t--){
+    solve();
+  }
+
+  return 0;
+}
